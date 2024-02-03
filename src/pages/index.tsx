3587460aff9a60b5b0aef8534a9d38408234b9f3
LilAliTech <lilalix20@gmail.com>
2024-02-03 18:57:50 +0000

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <p className="text-black">Landing page with parallax smooth scroll.</p>
+    </main>
+  );
+}
