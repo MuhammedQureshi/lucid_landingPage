e259b5b16c04dfde2b05609735f1429fdc59c023
LilAliTech <lilalix20@gmail.com>
2024-02-05 20:15:45 +0000

    Add Nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
+import Logo from '@/../public/logo.svg';
+import Link from 'next/link';
+
 function Navbar() {
+
+    const routes = [
+        { href: '#', label: 'Air' },
+        { href: '#', label: 'Gravity' }, 
+        { href: '#', label: 'Experience' }, 
+        { href: '#', label: 'Shop' }, 
+    ]
   return (
-    <div>Navbar</div>
+    <nav className='absolute inset-0 top-0 h-16 flex justify-between items-center px-20 bg-gradient-to-b from-black to-black/0'>
+        <Logo className='w-60' />
+        <div className='flex gap-6'>
+            {routes.map(route => (
+                <Link key={route.href} href={route.href}>
+                    {route.label}
+                </Link>
+            ))}
+        </div>
+    </nav>
   )
 }
 
